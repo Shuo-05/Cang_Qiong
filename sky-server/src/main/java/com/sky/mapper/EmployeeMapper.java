b7e366cde6501bc694a938128fce3dab02a143ea
Shuo <2191635716@qq.com>
2025-09-13 17:16:29 +0800

    Day2

--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -1,6 +1,11 @@
 package com.sky.mapper;
 
+import com.github.pagehelper.Page;
+import com.sky.annocation.AutoFill;
+import com.sky.dto.EmployeePageQueryDTO;
 import com.sky.entity.Employee;
+import com.sky.enumeration.OperationType;
+import com.sky.result.PageResult;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
@@ -22,6 +27,20 @@ public interface EmployeeMapper {
      */
     @Insert("insert into employee(name, username, password, phone, sex, id_number, status, create_time, update_time, create_user, update_user) " +
             "values " + "(#{name},#{username},#{password},#{phone},#{sex},#{idNumber},#{status},#{createTime},#{updateTime},#{createUser},#{updateUser})")
-
+    @AutoFill(value = OperationType.INSERT)
     void insert(Employee employee);
+
+    /**
+     * 分页查询
+     * @param employeePageQueryDTO
+     * @return
+     */
+    Page<Employee> selectemployee(EmployeePageQueryDTO employeePageQueryDTO);
+
+    void startorStop(Employee employee);
+
+    @Select("select * from employee where id=#{id}")
+    Employee getById(Long id);
+    @AutoFill(value = OperationType.UPDATE)
+    void update(Employee employee);
 }
